e78a4b08986fab25fe08f2a294a21eff5c97ccde
Sergey Pravdyukov <sergey.pravdyukov@gmail.com>
2015-12-27 18:57:35 +0400

    — removed arrays of char; — added char*;

--- term_1/hw9/2.1/2.1/main.cpp
+++ term_1/hw9/2.1/2.1/main.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
+#include <string.h>
+#include <cstdio>
 #include <cstring>
+#include <string>
 #include <fstream>
 
 using namespace std;
 
-void input(char firstString[], char secondString[])
+struct String
 {
-	ifstream in("input.txt");
-	in >> firstString;
-	in >> secondString;
-	in.close();
+	char *data;
+	int size;
+};
+
+String createString(int size, int capacity)
+{
+	String newString = { nullptr, size };
+	return newString;
 }
 
-void calculatePrimePower(unsigned long long primeNumberInPower[], char firstString[])
+void input(String &currentString)
+{
+	const int capacity = int(1e3);
+	char *newString = new char[capacity];
+	cin.getline(newString, capacity);
+	currentString.size = strlen(newString);
+	currentString.data = newString;
+}
+
+void calculatePrimePower(unsigned long long primeNumberInPower[], String firstString)
 {
 	const int primeNumber = 37;
 	primeNumberInPower[0] = 1;
-	for (int i = 1; i < strlen(firstString); ++i)
+	for (int i = 1; i < firstString.size; ++i)
 	{
 		primeNumberInPower[i] = primeNumberInPower[i - 1] * primeNumber;
 	}
 }
 
-void haveHashOfFirstString(unsigned long long hashOfFirstString[], 
-	unsigned long long primeNumberInPower[], char firstString[])
+void haveHashOfFirstString(unsigned long long hashOfFirstString[],
+	unsigned long long primeNumberInPower[], String firstString)
 {
 	hashOfFirstString[0] = 0;
-	for (int i = 0; i <= strlen(firstString); ++i)
+	for (int i = 0; i <= firstString.size; ++i)
 	{
-		hashOfFirstString[i] = primeNumberInPower[i] * firstString[i];
+		hashOfFirstString[i] = primeNumberInPower[i] * firstString.data[i];
 		if (i)
 		{
 			hashOfFirstString[i] += hashOfFirstString[i - 1];
@@ -37,21 +53,29 @@ void haveHashOfFirstString(unsigned long long hashOfFirstString[],
 }
 
 void haveHashOfSecondString(unsigned long long &hashOfSecondString,
-	unsigned long long primeNumberInPower[], char secondString[])
+	unsigned long long primeNumberInPower[], String secondString)
 {
-	for (int i = 0; i < strlen(secondString); ++i)
+	for (int i = 0; i < secondString.size; ++i)
 	{
-		hashOfSecondString += primeNumberInPower[i] * secondString[i];
+		hashOfSecondString += primeNumberInPower[i] * secondString.data[i];
 	}
 }
 
-bool isFindSubstring(char firstString[], char secondString[], unsigned long long hashOfFirstString[],
+void debugOutput(String currentString)
+{
+	for (int i = 0; i < currentString.size; ++i)
+	{
+		cout << currentString.data[i];
+	}
+}
+
+bool isFindSubstring(String firstString, String secondString, unsigned long long hashOfFirstString[],
 	unsigned long long hashOfSecondString, unsigned long long primeNumberInPower[])
 {
 	bool result = false;
-	for (int i = 0; i + strlen(secondString) - 1 < strlen(firstString); ++i)
+	for (int i = 0; i + secondString.size - 1 < firstString.size; ++i)
 	{
-		unsigned long long currentHash = hashOfFirstString[i + int(strlen(secondString)) - 1];
+		unsigned long long currentHash = hashOfFirstString[i + secondString.size - 1];
 		if (i)
 		{
 			currentHash -= hashOfFirstString[i - 1];
@@ -60,7 +84,11 @@ bool isFindSubstring(char firstString[], char secondString[], unsigned long long
 		{
 			if (result == false)
 			{
-				cout << "Строка " << secondString << " является подстрокой строки " << firstString << " в позициях ";
+				cout << "Строка ";
+				debugOutput(secondString);
+				cout << " является подстрокой строки ";
+				debugOutput(firstString);
+				cout << " в позициях ";
 			}
 			result = true;
 			cout << i + 1 << " ";
@@ -69,31 +97,34 @@ bool isFindSubstring(char firstString[], char secondString[], unsigned long long
 	return result;
 }
 
-void output(char currentString[])
-{
-	for (int i = 0; i < strlen(currentString); ++i)
-	{
-		cout << currentString[i];
-	}
-}
-
 int main()
 {
 	setlocale(LC_CTYPE, "rus");
-	const int maxSize = 1e2;
-	char firstString[maxSize];
-	char secondString[maxSize];
-	input(firstString, secondString);
-	unsigned long long primeNumberInPower[maxSize];
+	const int size = 0;
+	const int capacity = int(1e3);
+	String firstString = createString(size, capacity);
+	String secondString = createString(size, capacity);
+	cout << "Введите строку: ";
+	input(firstString);
+	cout << "Введите подстроку: ";
+	input(secondString);
+	unsigned long long primeNumberInPower[capacity];
 	calculatePrimePower(primeNumberInPower, firstString);
-	unsigned long long hashOfFirstString[maxSize];
+	unsigned long long hashOfFirstString[capacity];
 	haveHashOfFirstString(hashOfFirstString, primeNumberInPower, firstString);
 	unsigned long long hashOfSecondString = 0;
 	haveHashOfSecondString(hashOfSecondString, primeNumberInPower, secondString);
 	if (!isFindSubstring(firstString, secondString, hashOfFirstString, hashOfSecondString, primeNumberInPower))
 	{
-		cout << "Строка " << secondString << " не является подстрокой строки " << firstString;
+		cout << "Строка ";
+		debugOutput(secondString);
+		cout << " не является подстрокой строки ";
+		debugOutput(firstString);
 	}
 	cout << endl;
 	return 0;
 }
+/*
+asdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhghasdasdffsdfgdhfghjfjhgkjgkfjgdhgh
+asdasdffsdfgdhfghjfjhgkjgkfjgdhgh
+*/
