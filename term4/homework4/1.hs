934fc76e6c346682441f14dca688b7dd74060f28
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2017-04-03 11:59:23 +0300

    Fixed bug

--- term4/homework4/1.hs
+++ term4/homework4/1.hs
@@ -1,5 +1,5 @@
-evens :: Integral a => [a] -> [a]
-evens l = filter (\x -> even x) l
+evens :: Integral a => [a] -> Int
+evens l = length $ filter (\x -> even x) l
 
 evens' :: Integral a => [a] -> a
 evens' l = sum (map (\x -> 1 - (x `mod` 2)) l)
