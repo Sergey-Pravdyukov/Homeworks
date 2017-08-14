1b86b45857675ce06a9b451e3391bb2655d63561
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2017-08-14 19:00:35 +0300

    Replace non-monadic with monadic functions

--- term4/homework5/3.hs
+++ term4/homework5/3.hs
@@ -5,8 +5,18 @@ last' l  = last l
 maxAmongNeighbsList :: [Int] -> [Int]
 maxAmongNeighbsList list = do
     x <- list
-    True <- return ((last' (takeWhile (/= x) list)) < x && last' (takeWhile (/= x) (reverse list)) < x)
+    True <- return ((last' (takeWhile' (/= x) list)) < x && last' (takeWhile' (/= x) (reverse' list)) < x)
     return (x)
 
 maxAmongNeighbs :: [Int] -> Int
 maxAmongNeighbs = head . maxAmongNeighbsList
+
+reverse' :: [Int] -> [Int]
+reverse' l = do
+    True <- return (l /= [])
+    ((reverse' (tail l)) ++ [head l])
+
+takeWhile' :: (Int -> Bool) -> [Int] -> [Int]
+takeWhile' p l = do
+    True <- return (l /= [] && p (head l))
+    (head l) : (takeWhile' p (tail l))
