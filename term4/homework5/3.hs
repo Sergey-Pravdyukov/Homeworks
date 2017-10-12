f98ebb35651a632bd6334b92755b900abfb62a5d
Sergey-Pravdyukov <sergey.pravdyukov@gmail.com>
2017-10-12 21:03:11 +0300

    fix getMax function

--- term4/homework5/3.hs
+++ term4/homework5/3.hs
@@ -4,6 +4,6 @@ maxAmongNeighbors :: [Int] -> Maybe Int
 maxAmongNeighbors list = foldl mplus Nothing (getMax list)
 
 getMax :: [Int] -> [Maybe Int]
-getMax _                                                                       = []
 getMax (previous:current:next:elements) | previous < current && current > next = Just current : getMax (current:next:elements)
                                         | otherwise                            = Nothing : getMax (current:next:elements)
+getMax _                                                                       = []
